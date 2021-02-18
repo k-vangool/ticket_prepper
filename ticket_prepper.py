f8b0f794f37cd66cbf9d85edc01035d41e119552
k-vangool <k.van.gool93@gmail.com>
2021-02-18 20:03:24 +0100

    Started on a GUI for obtaining the ticket location.

--- ticket_prepper.py
+++ ticket_prepper.py
@@ -1,14 +1,17 @@
-import webbrowser as wb
-import pandas as pd
+import datetime as dt
 import os
 import time
-import datetime as dt
+import webbrowser as wb
+import pandas as pd
+from ticket_prepper_gui import *
 
 # Importing tickets
-file_name = 'input_tickets.xlsx'
+# file_name = 'input_tickets.xlsx'
 path = os.getcwd()
 
-tickets = pd.read_excel(os.path.join(path, file_name), engine='openpyxl')
+rungui()
+
+tickets = pd.read_excel(filepathExcel, engine='openpyxl')
 chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
 
 lst_of_cats = [
@@ -23,22 +26,25 @@ relevant_tickets.reset_index(inplace=True)
 relevant_tickets.dropna(1, inplace=True)
 relevant_tickets.drop(columns=['index'], inplace=True)
 
-wb.register('chrome', None, wb.BackgroundBrowser(chrome_path))
-chrome = wb.get('chrome')
+def open_tickets():
+    wb.register('chrome', None, wb.BackgroundBrowser(chrome_path))
+    chrome = wb.get('chrome')
 
-os.startfile(chrome_path, 'open')
+    os.startfile(chrome_path, 'open')
 
-url_str_no_ticket = 'https://edgeos.edgeconnex.com/portal/Forms/ShowRequest.aspx?ID_FINDER='
+    url_str_no_ticket = 'https://edgeos.edgeconnex.com/portal/Forms/ShowRequest.aspx?ID_FINDER='
 
-for i in range(len(relevant_tickets['Ticket #'])):
-    new_url = url_str_no_ticket + str(relevant_tickets['Ticket #'][i])
+    for i in range(len(relevant_tickets['Ticket #'])):
+        new_url = url_str_no_ticket + str(relevant_tickets['Ticket #'][i])
 
-    chrome.open_new_tab(new_url)
-    time.sleep(0.095)
+        chrome.open_new_tab(new_url)
+        time.sleep(0.095)
 
+open_tickets()
 
 # Saving the new tickets
 relevant_tickets.set_index(['Ticket #'], inplace=True)
 date = str(dt.datetime.today().strftime('%d-%m-%Y'))
 new_file_name = 'tickets_' + date + '.xlsx'
 relevant_tickets.to_excel(os.path.join(path, new_file_name), engine='openpyxl')
+
